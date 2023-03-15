d353b38dbd391ebf2fcd3641e12ac388418e8064
Vitaliy <you@example.com>
2023-03-15 19:05:17 +0200

    Route exact

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Table from './components/Table/Table';
+import Home from './components/Home/Home';
 
 const App: React.FC = () => (
-  <div className="App" data-testid="App">
-    <span>Test</span>
+  <div className='App' data-testid='App'>
+    <Router basename='/https://Vitaliy-Mazurenko.github.io/typescript/'>
+      <Routes>
+        <Route path='/table' element={<Table />} />
+        <Route exact path='/' element={<Home />} />
+      </Routes>
+    </Router>
   </div>
 );
 
