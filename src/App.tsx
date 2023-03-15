aa96c5bac21e586d0bd6b4c3d4216f5179d7c9e7
Vitaliy <you@example.com>
2023-03-15 19:21:08 +0200

    fix: BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Table from './components/Table/Table';
 import Home from './components/Home/Home';
 
 const App: React.FC = () => (
   <div className="App" data-testid="App">
-    <Router basename="/https://Vitaliy-Mazurenko.github.io/typescript/">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/table" element={<Table />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/table" element={<Table />} />
+    </Routes>
   </div>
 );
 
